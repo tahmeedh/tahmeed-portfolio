2b7df38cb3f5fb1ca22d7460762500edc3a1d01c
tahmeedh <60047767+tahmeedh@users.noreply.github.com>
2026-04-10 04:03:07 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  server: {
-    port: 4321,
-  },
+  output: 'static',
 });
